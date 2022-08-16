1803ba7f41f4791c261f85839fabb4b441b75aaa
Simeon Kirov <simeonkirov21@gmail.com>
2022-08-16 18:54:05 +0300

    Added 06.SumOfNumbersN…M

---
+++ SyntaxFunctionsAndStatementsLab/06.SumOfNumbersN…M.js
@@ -0,0 +1,13 @@
+function FindSum(n, m) {
+    let start = Number(n);
+    let end = Number(m);
+
+    let result = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
+}
+
