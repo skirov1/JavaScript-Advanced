7402193b295fa053847efdf1cb6e6ff8962b8bb2
Simeon Kirov <simeonkirov21@gmail.com>
2022-08-16 18:52:41 +0300

    Added 02.StringLength

---
+++ SyntaxFunctionsAndStatementsLab/02.StringLength.js
@@ -0,0 +1,9 @@
+function strlen(a, b, c){
+    let len1 = a.length;
+    let len2 = b.length;
+    let len3 = c.length;
+
+    let total = len1 + len2 + len3;
+    console.log(total);
+    console.log(Math.floor(total / 3));
+}
