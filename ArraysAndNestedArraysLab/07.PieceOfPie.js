9f437a583bd38acc20c2c27b75dc0a5cd828ad60
Simeon Kirov <simeonkirov21@gmail.com>
2022-08-31 18:15:00 +0300

    Added 07.PieceOfPie

---
+++ ArraysAndNestedArraysLab/07.PieceOfPie.js
@@ -0,0 +1,8 @@
+function Pie(arr, firstFlavor, secondFlavor) {
+
+    let firstIndex = arr.indexOf(firstFlavor);
+    let secondIndex = arr.indexOf(secondFlavor);
+
+    return arr.slice(firstIndex, secondIndex + 1);
+
+}
