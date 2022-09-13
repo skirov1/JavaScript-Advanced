1a102af16fdc9c438b6336b4d494c6dc38585964
Simeon <simeonkirov21@gmail.com>
2022-09-13 14:53:15 +0300

    Added 06.StoreCatalogue

---
+++ ObjectsAndCompositionExercise/06.StoreCatalogue.js
@@ -0,0 +1,26 @@
+function solve(input) {
+    let obj = {};
+    
+    for (const line of input) {
+        let [product, price] = line.split(' : ');
+        let letter = product[0];
+
+        if (!obj.hasOwnProperty(letter)){
+            obj[letter] = {}
+        }
+
+        obj[letter][product] = price;
+    }
+
+    let sortedLetters = Object.keys(obj).sort((a,b) => a.localeCompare(b));
+
+    for (const letter of sortedLetters) {
+        console.log(letter);
+        let sortedProducts = Object.keys(obj[letter]).sort((a,b) => a.localeCompare(b));
+
+        for (const product of sortedProducts) {
+            console.log(`  ${product}: ${obj[letter][product]}`)
+        }
+
+    }
+}
