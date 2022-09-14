35ec0543fa3cb60f8d550a23dedf0f493868d664
Simeon <simeonkirov21@gmail.com>
2022-09-14 16:21:09 +0300

    Added 06.SumTable

---
+++ DOMIntroductionLab/06.Sum Table/SumTable.js
@@ -0,0 +1,11 @@
+function sumTable() {
+    const rows = Array.from(document.querySelectorAll('tr')).slice(1 ,-1);
+
+    let sum = 0;
+
+    for (let row of rows) {
+        sum += Number(row.lastElementChild.textContent);
+    }
+    
+    document.getElementById('sum').textContent = sum;
+}
