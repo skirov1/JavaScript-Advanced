08963bb418dcfdeca0ed14eace7c779e82b1e03b
Simeon <simeonkirov21@gmail.com>
2022-10-02 13:52:17 +0300

    Added 05.CommandProcessor

---
+++ AdvancedFunctionsLab/05.CommandProcessor.js
@@ -0,0 +1,19 @@
+function solution() {
+    let text = '';
+
+    return {
+        append : (string) =>  text = text += string,
+        removeStart : (n) => text = text.substring(n),
+        removeEnd : (n) => text = text.substring(0, text.length - n),
+        print : () => console.log(text)
+    }
+    
+}
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
