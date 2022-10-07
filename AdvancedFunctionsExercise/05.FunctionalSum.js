6a45ae3ae7ad352d54dab59f50f07c361ecd39e9
Simeon <simeonkirov21@gmail.com>
2022-10-07 21:30:29 +0300

    Added 05.FunctionalSum

---
+++ AdvancedFunctionsExercise/05.FunctionalSum.js
@@ -0,0 +1,9 @@
+function add(a){
+    function sum(b){
+       a = a+b;
+       return sum;
+    }
+    sum.toString = function(){return a;}
+    return sum;
+}
+   console.log(add(1)(2)(3)(4).toString());
