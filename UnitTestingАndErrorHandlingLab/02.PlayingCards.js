22bf7eaff083a965faf6c1fe09b8d160946e6c08
Simeon <simeonkirov21@gmail.com>
2022-10-12 09:23:14 +0300

    Added 02.PlayingCards

---
+++ UnitTestingАndErrorHandlingLab/02.PlayingCards.js
@@ -0,0 +1,27 @@
+function solve(face, suit) {
+    const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const validSuits = ['S', 'H', 'D', 'C'];
+
+    if (validFaces.indexOf(face.toString()) === -1){
+        throw new Error;
+    }
+
+    if (validSuits.indexOf(suit.toString()) === -1){
+        throw new Error;
+    }
+
+    switch (suit) {
+        case 'S': suit = '\u2660'; break;
+        case 'H': suit = '\u2665'; break;
+        case 'D': suit = '\u2666'; break;
+        case 'C': suit = '\u2663'; break;
+    }
+
+    return {
+        face : face,
+        suit : suit,
+        toString() {
+            return this.face + this.suit;
+        }
+    }
+}
